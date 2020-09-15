8da43ade36541e4bc87c80cf274f04fde953e394
zuqq <noahheld17@gmail.com>
2020-09-15 23:10:35 +0200

    Branches can be empty

--- src/main/java/record/Branch.java
+++ src/main/java/record/Branch.java
@@ -1,23 +1,26 @@
 package record;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public class Branch {
-    private Commit tip;
+    private Optional<Commit> optionalTip;
 
     public Branch(Commit tip) {
-        this.tip = tip;
+        this.optionalTip = Optional.of(tip);
     }
 
-    public Commit getTip() {
-        return tip;
+    public Optional<Commit> getTip() {
+        return optionalTip;
     }
 
     public void setTip(Commit tip) {
-        this.tip = tip;
+        this.optionalTip = Optional.of(tip);
     }
 
-    public byte[] getBytes() {
-        return Base16.encode(tip.getBytes()).getBytes(StandardCharsets.UTF_8);
+    public Optional<byte[]> getBytes() {
+        return optionalTip.map(
+            tip -> Base16.encode(tip.getBytes()).getBytes(StandardCharsets.UTF_8)
+        );
     }
 }
