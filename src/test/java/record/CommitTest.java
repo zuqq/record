e4f1fe2291453b05e3512e03c2f3244bd82e0c3c
zuqq <noahheld17@gmail.com>
2020-10-13 16:45:47 +0200

    Set up tests consistently

--- src/test/java/record/CommitTest.java
+++ src/test/java/record/CommitTest.java
@@ -8,38 +8,32 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CommitTest {
-    private static Commit initialCommit = null;
-    private static Commit secondCommit = null;
-
-    @BeforeAll
-    public static void setUp() {
-        User user = new User("Jane Doe", "jane@example.com");
-        Timestamp initialTimestamp = Timestamp.of("1599568789 +0200");
-        initialCommit = new Commit(
-            "3683f870be446c7cc05ffaef9fa06415276e1828",
-            new ArrayList<>(),
-            user,
-            initialTimestamp,
-            user,
-            initialTimestamp,
-            "Initial commit"
-        );
-        Timestamp secondTimestamp = Timestamp.of("1599568810 +0200");
-        secondCommit = new Commit(
-            "5e1dd7430fe0d9b1678543ae1a318485d69fdd2c",
-            Collections.singletonList(Base16.encode(initialCommit.getHash())),
-            user,
-            secondTimestamp,
-            user,
-            secondTimestamp,
-            "Add more stuff"
-        );
-    }
+    private final User user = new User("Jane Doe", "jane@example.com");
+    private final Timestamp firstTimestamp = Timestamp.of("1599568789 +0200");
+    private final Commit firstCommit = new Commit(
+        "3683f870be446c7cc05ffaef9fa06415276e1828",
+        new ArrayList<>(),
+        user,
+        firstTimestamp,
+        user,
+        firstTimestamp,
+        "Initial commit"
+    );
+    private final Timestamp secondTimestamp = Timestamp.of("1599568810 +0200");
+    private final Commit secondCommit = new Commit(
+        "5e1dd7430fe0d9b1678543ae1a318485d69fdd2c",
+        Collections.singletonList(Base16.encode(firstCommit.getHash())),
+        user,
+        secondTimestamp,
+        user,
+        secondTimestamp,
+        "Add more stuff"
+    );
 
     @Test
     void getHash() {
         Assertions.assertEquals(
-            "42a22126b2d4fef6dd6537ecad0e63be1bc4c210", Base16.encode(initialCommit.getHash())
+            "42a22126b2d4fef6dd6537ecad0e63be1bc4c210", Base16.encode(firstCommit.getHash())
         );
         Assertions.assertEquals(
             "79d5cd29da3d56d9abda7e83d2b2bd52d43db939", Base16.encode(secondCommit.getHash())
